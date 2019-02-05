10f5e17677092ed1d0ba803c710b939ae5bf88c5
Adriano Maringolo Gomes <adrianomaringolo@gmail.com>
2019-02-05 15:38:24 -0200

    Componentes statefull e stateless

--- src/components/JobsList/JobCard/JobCard.js
+++ src/components/JobsList/JobCard/JobCard.js
@@ -1,30 +1,49 @@
 import React from 'react';
+
 import developerImg from '../../../assets/developer.png'
+import testerImg from '../../../assets/tester.png'
+import designerImg from '../../../assets/designer.png'
 
-const jobCard = () => (
-  <div className="col-sm-12 col-md-6 col-lg-4 mb-3">
-  <div className="card">
-    <img className="card-img-top" src={developerImg} alt="Card image cap" />
-    <div className="card-body">
-      <h5 className="card-title">Desenvolvedor Front-end JR</h5>
-      <div>
-        <b>Descrição:</b>
-        <p>Profissional que goste de trabalhar em um ambiênte dinâmico com desenvolvimento de software e que tenha experiência
-            em desenvolvimento full stack.</p>
+const jobCard = (props) => {
 
-        <b>Salário base:</b>
-        <p>R$ 10,00</p>
+  let img = null;
 
+  switch( props.area ) {
+    case 'dev':
+      img = developerImg;
+      break;
+    case 'test':
+      img = testerImg;
+      break;
+    case 'design':
+      img = designerImg;
+      break;
+  }
+
+  return (
+    <div className="col-sm-12 col-md-6 col-lg-4 mb-3">
+    <div className="card">
+      <img className="card-img-top" src={ img } alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">{ props.name }</h5>
+        <div>
+          <b>Descrição:</b>
+          <p>{ props.description }</p>
+
+          <b>Salário base:</b>
+          <p>R$  { props.salary }</p>
+
+        </div>
+        <a href="#" className="btn btn-warning">
+          <i className="far fa-edit"></i>
+        </a>
+        <button onClick={ props.removeHandler } className="btn btn-danger">
+          <i className="far fa-trash-alt"></i>
+        </button>
       </div>
-      <a href="#" className="btn btn-warning">
-        <i className="far fa-edit"></i>
-      </a>
-      <a href="#" className="btn btn-danger">
-        <i className="far fa-trash-alt"></i>
-      </a>
     </div>
-  </div>
-  </div>
-)
+    </div>
+  )
+}
 
 export default jobCard;
--- src/components/JobsList/JobsList.js
+++ src/components/JobsList/JobsList.js
@@ -2,13 +2,42 @@ import React from 'react';
 
 import JobCard from './JobCard/JobCard';
 
-const jobsList = () => (
-  <div className="row">
-    <JobCard/>
-    <JobCard/>
-    <JobCard/>
+class JobsList extends React.Component {
 
-  </div>
-);
+  state = {
+    jobs: [
+      { name: 'Desenvolvedor JR', description: 'jahsdkjahskdj', salary: 1200, area: 'dev' },
+      { name: 'Tester JR', description: 'blablabla', salary: 1200, area: 'test' }
+    ],
+    hasError: false
+  }
 
-export default jobsList;
+  jobRemoveHandler = (paramName) => {
+    if (window.confirm(`Deseja realmente remover a vaga "${paramName}"?`)) {
+
+      //let newList = ...;
+      //this.setState({ jobs: newList });
+
+      window.alert('Removido com sucesso!');
+    }
+  }
+
+  render() {
+
+    const renderJobs = this.state.jobs.map(job => {
+      return <JobCard
+        name={job.name}
+        description={job.description}
+        salary={job.salary}
+        area={job.area}
+        removeHandler={() => this.jobRemoveHandler(job.name)} />
+    });
+
+    return (
+      <div className="row">
+        {renderJobs}
+      </div>
+    )
+  }
+}
+export default JobsList;
